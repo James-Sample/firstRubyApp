de763bd32c039c166bda781a5c84f79219bbdd80
James Sample <jamesgsample@gmail.com>
2023-05-02 18:45:04 +0100

    added create function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+      # If the article is saved successfully, the action redirects the browser to the article's page at "http://localhost:3000/articles/#{@article.id}
+    else
+      render :new, status: :unprocessable_entity  
+      # the action redisplays the form by rendering app/views/articles/new.html.erb with status code 422 Unprocessable Entity.
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
